5d20428e80c61d81344e1618647363451b704331
irminaj <irminaj@github.com>
2023-07-24 00:13:58 +0300

    modify button component style

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { ButtonStyle, SocialIconStyle } from "./Button.style";
 
-export const Button = ({ iconSrc, text }) => {
+export const Button = ({ iconSrc, text, buttonVariant }) => {
   return (
-    <ButtonStyle>
+    <ButtonStyle variant={buttonVariant}>
       <SocialIconStyle src={iconSrc} />
       {text}
     </ButtonStyle>
--- src/components/button/Button.style.jsx
+++ src/components/button/Button.style.jsx
@@ -1,15 +1,32 @@
-import { styled } from "styled-components";
+import { styled, css } from "styled-components";
 
 export const ButtonStyle = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 5px 0;
-  width: 140px;
+  padding: 8px 0;
   border-radius: 5px;
+  font-size: 20px;
+  font-weight: 400;
+  width: 45%;
+  ${(props) => {
+    if (props.variant === "fbButton") {
+      return css`
+        background-color: ${(props) => props.theme.colors.blue};
+        color: ${(props) => props.theme.colors.white};
+        border: none;
+      `;
+    } else if (props.variant === "googleButton") {
+      return css`
+        background-color: ${(props) => props.theme.colors.white};
+        border: 3px solid;
+        border-color: ${(props) => props.theme.colors.steelGrey};
+      `;
+    }
+  }}
 `;
 
 export const SocialIconStyle = styled.img`
-  width: 20px;
+  width: 28px;
   padding-right: 5px;
 `;
