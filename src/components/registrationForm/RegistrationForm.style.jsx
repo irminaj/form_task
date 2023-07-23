a9f47a752e02ed758d6c3593d58e8c51e1c49eea
irminaj <irminaj@github.com>
2023-07-24 00:17:18 +0300

    modify Registration form component style

--- src/components/registrationForm/RegistrationForm.style.jsx
+++ src/components/registrationForm/RegistrationForm.style.jsx
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
 export const RegistrationFormContainerStyle = styled.div`
+  width: 80%;
+  margin: 40px auto;
   display: flex;
+  border: 2px solid red;
+  border-radius: 10px;
 `;
